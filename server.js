d6126fb85bebeafb9c40591d66e58ddabf18c2dd
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-01-30 14:56:06 +0100

    port veranderen

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port 3000');
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Listening on port 4000');
 });
 
 app.set('view engine', 'ejs');
