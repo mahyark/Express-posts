53381083fe68a1b377fc49dcca45f0a6413cbd89
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2018-01-30 15:48:30 +0100

    comments weergeven

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var request = require('request');
+var posts;
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Listening on port 4000');
@@ -21,23 +22,29 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
 	request("http://jsonplaceholder.typicode.com/posts", function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
-	    var posts = JSON.parse(body);
-
-	    /*for(var post in posts) {
-		   console.log(post.title);
-		}*/
-
-		console.log(JSON.stringify(posts[2].title));
+	    posts = JSON.parse(body);
    		res.render('posts.ejs', { posts: posts });
 	  }
-	  
 	});
 });
 
 // Show the search form
 app.get('/search', (req, res) => {
-   res.render('search.ejs', { post: '' });
+   res.render('search.ejs', { post: posts });
 });
 
 // Find all comments for post
-app.post('/search', (req, res) => {});
+app.post('/search', (req, res) => {
+	var id;
+	for (var i = posts.length - 1; i >= 0; i--) {
+		if (posts[i].title == req.body.title) {
+			id = posts[i].id;
+			request("http://jsonplaceholder.typicode.com/posts/" + id + "/comments", function (error, response, body) {
+			  if (!error && response.statusCode == 200) {
+			    comments = JSON.parse(body);
+   				res.render('search_result.ejs', { comments: comments }); 
+			  }
+			});
+		} 
+	}	
+});
